c6a32443e912ca8035bb5cb4893a9920177f2109
auooru <auooru@outlook.com>
2021-03-07 21:26:15 +0800

    init commit

---
+++ src/Schema/IndexDefinition.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: NHZEXG
+ * Date: 2019/2/23
+ * Time: 10:07
+ */
+
+namespace Zxin\PhinxExt\Schema;
+
+use ValueError;
+use function strtoupper;
+
+class IndexDefinition
+{
+    protected $options = [];
+
+    /**
+     * @param string|string[] $field
+     * @return string
+     */
+    public static function generateName($field)
+    {
+        if (is_array($field)) {
+            $name = implode('_', $field);
+        } else {
+            $name = $field;
+        }
+        return $name;
+    }
+
+    /**
+     * @var string|string[]|null
+     */
+    protected $field = null;
+
+    /**
+     * IndexDefinition constructor.
+     * @param string|string[]|null $field
+     */
+    public function __construct($field = null)
+    {
+        $this->field = $field;
+    }
+
+    /**
+     * 是否唯一
+     * @param bool $value
+     * @return $this
+     */
+    public function unique(bool $value = true): IndexDefinition
+    {
+        $this->options['unique'] = $value;
+        return $this;
+    }
+
+    /**
+     * 索引长度
+     * @param int|array $value
+     * @return $this
+     */
+    public function limit($value): IndexDefinition
+    {
+        $this->options['limit'] = $value;
+        return $this;
+    }
+
+    /**
+     * 索引名称
+     * @param string $name
+     * @return $this
+     */
+    public function name(string $name): IndexDefinition
+    {
+        $this->options['name'] = $name;
+        return $this;
+    }
+
+    /**
+     * 设置主键字段为 UNSIGNED
+     * @param bool $enable
+     * @return $this
+     */
+    public function unsigned(bool $enable = true): IndexDefinition
+    {
+        $this->options['signed'] = !$enable;
+        return $this;
+    }
+
+    /**
+     * 定义表注释
+     * @param $text
+     * @return $this
+     */
+    public function comment(string $text): IndexDefinition
+    {
+        $this->options['comment'] = $text;
+        return $this;
+    }
+
+    /**
+     * 设置 fulltext 索引 (mysql)
+     * @param bool $enable
+     * @return $this
+     */
+    public function fulltext(bool $enable = true): IndexDefinition
+    {
+        if (!$enable && isset($this->options['type']) && 'fulltext' === $this->options['type']) {
+            unset($this->options['type']);
+        } else {
+            $this->options['type'] = 'fulltext';
+        }
+        return $this;
+    }
+
+    /**
+     * 定义表排序规则
+     * @param $value
+     * @return $this
+     */
+    public function collation(string $value): IndexDefinition
+    {
+        $this->options['collation'] = $value;
+        return $this;
+    }
+
+    /**
+     * @param array $order
+     * @return $this
+     */
+    public function order(array $order): IndexDefinition
+    {
+        foreach ($order as $field => $value) {
+            $value = strtoupper($value);
+            if ($value !== 'DESC' && $value !== 'ASC') {
+                throw new ValueError('order value can only be DESC or ASC');
+            }
+        }
+        $this->options['order'] = $order;
+        return $this;
+    }
+
+    /**
+     * @return string|string[]|null
+     */
+    public function getField()
+    {
+        return $this->field;
+    }
+
+    public function getOptions(): array
+    {
+        return $this->options;
+    }
+}
---
+++ src/func.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Zxin\PhinxExt;
+
+use function preg_replace;
+use function strtolower;
+
+/**
+ * @param string $input
+ * @return string
+ */
+function to_snake_case(string $input): string
+{
+    return strtolower(preg_replace('/([a-z])([A-Z])/', '$1_$2', $input));
+}
