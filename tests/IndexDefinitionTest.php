158eb3c2f9d5127a6bae919bf92f5cb62bfcd694
auooru <auooru@outlook.com>
2021-03-07 22:10:01 +0800

    add: tests

---
+++ tests/IndexDefinitionTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Zxin\Tests;
+
+use PHPUnit\Framework\TestCase;
+use ValueError;
+use Zxin\Phinx\Schema\Definition\IndexDefinition;
+
+class IndexDefinitionTest extends TestCase
+{
+    public function testOrder()
+    {
+        $this->expectException(ValueError::class);
+        $this->expectExceptionMessage('order value can only be DESC or ASC');
+
+        $inde = new IndexDefinition('index');
+        $inde->order([
+            'index' => 'ABC',
+        ]);
+    }
+}
